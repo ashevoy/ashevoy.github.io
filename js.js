199ea2a5e3b652a57445b3116ad2b9fa0570287a
ashevoy <deejaysixtytwo@protonmail.com>
2024-11-30 18:43:14 +0100

    added actual buttons and stuff

---
+++ js.js
@@ -0,0 +1,11 @@
+window.onload = choosePic;
+
+var ephBg = new Array("i/eph1.webp","i/eph2.webp","i/eph3.webp","i/eph4.webp","i/eph5.webp");
+var zomBg = new Array("i/zom1.webp","i/zom2.webp","i/zom3.webp","i/zom4.webp");
+
+function choosePic() {
+     var randomNum = Math.floor(Math.random() * ephBg.length);
+     var randomNumt = Math.floor(Math.random() * zomBg.length);
+     document.getElementById("ephPic").src = ephBg[randomNum];
+     document.getElementById("zomPic").src = zomBg[randomNumt];
+}
