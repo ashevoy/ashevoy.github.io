41e9191c02731999da03d7080867050a79baf43d
ashevoy <austioncontact@gmail.com>
2025-01-23 18:43:42 +0100

    eph updates site

--- jseph.js
+++ jseph.js
@@ -1,14 +1,25 @@
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+const modal = document.getElementById("imageModal");
+const modalImage = document.getElementById("modalImage");
+const closeBtn = document.querySelector(".closebtn");
+const thumbnails = document.querySelectorAll(".smallThumb");
 
-nextButton.addEventListener("click", () => {
-    const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft += slideWidth;
+thumbnails.forEach(smallThumb => {
+    console.log("Adding click listener to", smallThumb);
+    smallThumb.addEventListener("click", () => {
+        console.log("thumb clicked", smallThumb.src);
+        modal.style.display = "flex";
+        modalImage.src = smallThumb.src;
+        console.log("thumb clicked");
+    });
 });
 
-prevButton.addEventListener("click", () => {
-    const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft -= slideWidth;
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Close modal when clicking outside the image
+modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
 });
